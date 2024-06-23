028e15d16d8acc28b3939c6ab7d1dabbf0e428b1
reisuta <in345vain@gmail.com>
2024-06-23 17:51:08 +0900

    cors設定

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"lp-api/config"
 	"lp-api/handlers"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/joho/godotenv"
 	"log"
 )
@@ -19,6 +20,11 @@ func main() {
   config.SeedData()
 
 	e := echo.New()
+
+  e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3001"},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE},
+	}))
 
   e.POST("/save", handlers.SaveData)
   e.GET("/diagnosis/:id", handlers.GetDiagnosis)
