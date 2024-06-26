c9c5ce6efde6e36304d4c6168c027de37bf8660e
reisuta <in345vain@gmail.com>
2024-06-26 22:45:20 +0900

    質問のseedを作成

--- db/migrations/000001_create_initial_tables.down.sql
+++ db/migrations/000001_create_initial_tables.down.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS diagnoses_answers;
 DROP TABLE IF EXISTS diagnoses_questions_choices;
 DROP TABLE IF EXISTS diagnoses_questions;
+DROP TABLE IF EXISTS diagnoses_headers;
 DROP TABLE IF EXISTS diagnoses;
 DROP TABLE IF EXISTS users;
--- db/migrations/000001_create_initial_tables.up.sql
+++ db/migrations/000001_create_initial_tables.up.sql
@@ -1,3 +1,5 @@
+START TRANSACTION;
+
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
@@ -10,6 +12,13 @@ CREATE TABLE IF NOT EXISTS diagnoses (
   title VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS diagnoses_headers (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  diagnosis_id INT,
+  title VARCHAR(255) NOT NULL,
+  FOREIGN KEY (diagnosis_id) REFERENCES diagnoses(id) ON DELETE CASCADE
+);
+
 CREATE TABLE IF NOT EXISTS diagnoses_questions (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
@@ -17,7 +26,9 @@ CREATE TABLE IF NOT EXISTS diagnoses_questions (
   type VARCHAR(255) NOT NULL,
   is_valid BOOLEAN,
   diagnosis_id INT,
-  FOREIGN KEY (diagnosis_id) REFERENCES diagnoses(id) ON DELETE CASCADE
+  diagnosis_header_id INT,
+  FOREIGN KEY (diagnosis_id) REFERENCES diagnoses(id) ON DELETE CASCADE,
+  FOREIGN KEY (diagnosis_header_id) REFERENCES diagnoses_headers(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS diagnoses_questions_choices (
@@ -25,14 +36,18 @@ CREATE TABLE IF NOT EXISTS diagnoses_questions_choices (
   title VARCHAR(255) NOT NULL,
   point INT,
   is_valid BOOLEAN,
-  question_id INT,
-  FOREIGN KEY (question_id) REFERENCES diagnoses_questions(id) ON DELETE CASCADE
+  diagnosis_question_id INT,
+  FOREIGN KEY (diagnosis_question_id) REFERENCES diagnoses_questions(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS diagnoses_answers (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   point INT,
-  question_id INT,
-  FOREIGN KEY (question_id) REFERENCES diagnoses_questions(id) ON DELETE CASCADE
+  diagnosis_question_id INT,
+  user_id INT,
+  FOREIGN KEY (diagnosis_question_id) REFERENCES diagnoses_questions(id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+COMMIT;
