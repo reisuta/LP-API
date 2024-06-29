23c73878e3b0ca4665c71dfdcb20f484ad62b058
reisuta <in345vain@gmail.com>
2024-06-29 18:04:08 +0900

    仮ユーザーを作成するように出力する

--- compose-build-up.sh
+++ compose-build-up.sh
@@ -9,6 +9,9 @@ docker cp ./db/seed/seed.sql mysql:/tmp/seed.sql
 # docker compose exec -T db mysql < ./db/seed/seed.sql
 # docker compose exec -T db sh -c 'mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /tmp/seed.sql'
 
+echo '====================================='
 echo '下記の手順を実行'
 echo 'docker compose exec db /bin/bash'
 echo 'mysql -u ユーザー名 -p < /tmp/seed.sql'
+echo 'ログイン機能を実装するまではinsert into users (name, email) values ('test', 'test@example.com');も実行する'
+echo '====================================='
